bf3f21c2496730cd1a7cb064255eacac325639f9
andre.oliveira <andre.oliveira@funcate.org.br>
2017-11-24 13:04:54 -0200

    Lesson 4 - Exercise 3 Completed

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkerService.java
@@ -0,0 +1,18 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkerService extends FirebaseInstanceIdService {
+
+    private static final String LOG_TAG = SquawkerService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+    }
+}
